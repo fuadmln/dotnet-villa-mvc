1c06a4bc620704a299fbaae9c503d68ea366962a
Fuad Maulana <fuadm03@gmail.com>
2024-02-06 14:41:36 +0700

    feat: villa seeder migration

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -15,4 +15,39 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Villa> Villas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Villa>().HasData(
+            new Villa 
+            {
+                Id = 1,
+                Name = "Royal Villa",
+                Description = "Description of Royal Villa",
+                ImageUrl = "https://placehold.co/600x400",
+                Occupancy = 4,
+                price = 200,
+                Squarefeet = 550
+            },new Villa 
+            {
+                Id = 2,
+                Name = "Premium Pool Villa",
+                Description = "Description of Premium Pool Villa",
+                ImageUrl = "https://placehold.co/600x400",
+                Occupancy = 4,
+                price = 300,
+                Squarefeet = 580
+            },new Villa 
+            {
+                Id = 3,
+                Name = "Luxury Pool Villa",
+                Description = "Description of Luxury Pool Villa",
+                ImageUrl = "https://placehold.co/600x400",
+                Occupancy = 3,
+                price = 400,
+                Squarefeet = 720
+            }
+        );
+    }
 }
---
+++ WhiteLagoon.Infrastructure/Migrations/20240206073819_SeedVillaToDb.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVillaToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "CreatedDate", "Description", "ImageUrl", "Name", "Occupancy", "Squarefeet", "UpdatedDate", "price" },
+                values: new object[,]
+                {
+                    { 1, null, "Description of Royal Villa", "https://placehold.co/600x400", "Royal Villa", 4, 550, null, 200.0 },
+                    { 2, null, "Description of Premium Pool Villa", "https://placehold.co/600x400", "Premium Pool Villa", 4, 580, null, 300.0 },
+                    { 3, null, "Description of Luxury Pool Villa", "https://placehold.co/600x400", "Luxury Pool Villa", 3, 720, null, 400.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
