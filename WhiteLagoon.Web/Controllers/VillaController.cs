5b3248c3cbd4b84585b7cd06bfdcbc253bf7ac00
Fuad Maulana <fuadm03@gmail.com>
2024-02-13 21:18:49 +0700

    feat: add image in villa crud

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -4,9 +4,10 @@ using WhiteLagoon.Domain.Entities;
 
 namespace WhiteLagoon.Web.Controllers;
 
-public class VillaController(IUnitOfWork unitOfWork) : Controller
+public class VillaController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment) : Controller
 {
     private readonly IUnitOfWork _uniOfWork = unitOfWork;
+    private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment;
 
     public IActionResult Index()
     {
@@ -28,6 +29,25 @@ public class VillaController(IUnitOfWork unitOfWork) : Controller
         if (!ModelState.IsValid)
             return View();
 
+        if (villa.Image == null)
+            villa.ImageUrl = "https://placehold.co/600x400";
+        else
+        {
+            // TODO: file extension validation, in model
+            string imageFolder = @"images\VillaImages";
+			string saveFileName = Guid.NewGuid().ToString() + Path.GetExtension(villa.Image.FileName);
+            string saveFolder = Path.Combine(_webHostEnvironment.WebRootPath, imageFolder);
+
+            using (var fileStream = new FileStream(Path.Combine(saveFolder, saveFileName), FileMode.Create))
+            {
+                villa.Image.CopyTo(fileStream);
+            }
+
+            //TODO: check if file exists
+
+            villa.ImageUrl = @"\" + imageFolder + @"\" + saveFileName;
+        }
+
 		_uniOfWork.VillaRepo.Add(villa);
 		_uniOfWork.VillaRepo.Save();
         TempData["success"] = "The villa has been created successfully";
@@ -46,8 +66,33 @@ public class VillaController(IUnitOfWork unitOfWork) : Controller
 	[HttpPost]
 	public IActionResult Update(Villa villa)
 	{
-		if (!ModelState.IsValid)
+		if (!ModelState.IsValid || villa.Id < 1)
 			return View();
+
+		if (villa.Image != null)
+		{
+			// TODO: file extension validation, in model
+			string imageFolder = @"images\VillaImages";
+			string saveFileName = Guid.NewGuid().ToString() + Path.GetExtension(villa.Image.FileName);
+			string saveFolder = Path.Combine(_webHostEnvironment.WebRootPath, imageFolder);
+
+            if (!string.IsNullOrEmpty(villa.ImageUrl))
+            {
+                var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, villa.ImageUrl.TrimStart('\\'));
+
+                if (System.IO.File.Exists(oldImagePath))
+                    System.IO.File.Delete(oldImagePath);
+            }
+
+			using (var fileStream = new FileStream(Path.Combine(saveFolder, saveFileName), FileMode.Create))
+			{
+				villa.Image.CopyTo(fileStream);
+			}
+
+			//TODO: check if file exists
+
+			villa.ImageUrl = @"\" + imageFolder + @"\" + saveFileName;
+		}
 
 		_uniOfWork.VillaRepo.Update(villa);
 		_uniOfWork.VillaRepo.Save();
@@ -71,6 +116,14 @@ public class VillaController(IUnitOfWork unitOfWork) : Controller
 		Villa? villaDb = _uniOfWork.VillaRepo.Get(villa.Id);
         if (villaDb == null)
             return RedirectToAction("Error", "Home");
+
+		if (!string.IsNullOrEmpty(villaDb.ImageUrl))
+		{
+			var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, villaDb.ImageUrl.TrimStart('\\'));
+
+			if (System.IO.File.Exists(oldImagePath))
+				System.IO.File.Delete(oldImagePath);
+		}
 
 		_uniOfWork.VillaRepo.Remove(villaDb);
 		_uniOfWork.VillaRepo.Save();
